0e239d9985b6c00145bdb1fcf6e19ba76c543bdc
David Lucas Flayban <davidflayban2@hotmail.com>
2023-05-18 19:17:02 -0300

    Criação da classe pipe

---
+++ Code/pipe.py
@@ -0,0 +1,8 @@
+import pygame
+from constant import Constant
+from pygame.locals import *
+
+class Pipe(pygame.sprite.Sprite): #Criação da classe Pipe
+    def __init__(self):
+        
+    def update(self):    
