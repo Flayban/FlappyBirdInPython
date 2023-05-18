3973506dac7b4af66ba17fb5e3743410a410b50b
David Lucas Flayban <davidflayban2@hotmail.com>
2023-05-18 19:56:31 -0300

    Atualização na classe Constant

--- Code/constant.py
+++ Code/constant.py
@@ -2,7 +2,7 @@
 class Constant():
     # dimensões vertical e horizontal da tela em pixels
     SCREEN_WIDTH = 288 
-    SCREEN_HEIGTH = 512
+    SCREEN_HEIGTH = 600
 
     SPEED = 10 #Velocidade do Passaro
     GRAVITY = 1 #Gravidade do Passaro
@@ -14,7 +14,7 @@ class Constant():
     GROUND_HEIGTH = 100
 
     # dimensões vertical e horizontal do 'pipe' em pixels
-    PIPE_WIDTH = 120
+    PIPE_WIDTH = 80
     PIPE_HEIGTH = 500
 
     PIPE_GAP = 200 #distancia entre os 'pipes' em pixels
